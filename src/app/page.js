00c0ed31de33617b1efd77f9a745b88f01a4bcac
Umair Asim <umairasim0598@gmail.com>
2025-08-06 00:11:45 +0500

    Bug Fixes

--- src/app/page.js
+++ src/app/page.js
@@ -44,11 +44,11 @@ export default function Home() {
         <span className="mt-3 text-xl text-center font-bold bg-gradient-to-r from-[#5fabb9] to-[#d9d9d9] text-transparent bg-clip-text w-fit">
           Get to Know About us
         </span>
-        <span className=" my-8 text-center">Hi there! I\'m [Your Name], a data scientist specializing in data analytics, predictive modeling, natural language processing, machine learning, and AI chatbots. With a passion for unraveling insights from complex datasets, I\'m dedicated to helping businesses make informed decisions and stay ahead in today\'s data-driven world.
+        <span className=" my-8 text-center">Hi there! Im [Your Name], a data scientist specializing in data analytics, predictive modeling, natural language processing, machine learning, and AI chatbots. With a passion for unraveling insights from complex datasets, Im dedicated to helping businesses make informed decisions and stay ahead in todays data-driven world.
 
-          I bring a blend of technical expertise, hands-on experience, and a commitment to clear communication to every project. Whether it\'s uncovering hidden patterns, predicting future trends, or automating processes with AI, I\'m here to help you harness the full potential of your data.
+          I bring a blend of technical expertise, hands-on experience, and a commitment to clear communication to every project. Whether its uncovering hidden patterns, predicting future trends, or automating processes with AI, Im here to help you harness the full potential of your data.
 
-          Let\'s work together to transform your data into actionable insights that drive real results. Get in touch, and let\'s start unlocking the power of your data today!</span>
+          Lets work together to transform your data into actionable insights that drive real results. Get in touch, and lets start unlocking the power of your data today!</span>
         <TailwindBtn text={'Download Resume'} cssClass="w-fit" />
       </section>
 
